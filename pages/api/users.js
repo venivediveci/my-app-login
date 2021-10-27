77579d4a2153c54ebacdce7f44dadef1ef9c78de
coderhh <henry.h.hao@foxmail.com>
2021-10-27 21:22:34 +0800

    api change

--- pages/api/users.js
+++ pages/api/users.js
@@ -27,53 +27,70 @@ function createUser(db, email, password, callback) {
   })
 }
 
+/**
+ * POST: create new user
+ */
 const handler = (req, res) => {
-  if (req.method === 'POST') {
-    try {
-      assert.notEqual(null, req.body.email, 'Email required')
-      assert.notEqual(null, req.body.password, 'Password required')
-    } catch (bodyError) {
-      res.status(403).json({ error: true, message: bodyError.message })
-    }
+  const { method } = req
 
-    client.connect(function (err) {
-      assert.equal(null, err)
-      console.log('Connected to MongoDB server =>')
-      const db = client.db(dbName)
-      const email = req.body.email
-      const password = req.body.password
-
-      findUser(db, email, function (err, user) {
-        if (err) {
-          res.status(500).json({ error: true, message: 'Error finding User' })
-          return
-        }
-
-        if (!user) {
-          createUser(db, email, password, function (creationResult) {
-            console.log(creationResult)
-            if (creationResult.ops?.length === 1) {
-              const user = creationResult.ops[0]
-              const token = jwt.sign(
-                { userId: user.userId, email: user.email },
-                jwtSecret,
-                {
-                  expiresIn: '1m',
-                }
-              )
-              res.status(200).json({ token })
-              return
-            }
-          })
-        } else {
-          res.status(403).json({ error: true, message: 'Email exists' })
-          return
-        }
-      })
-    })
-  } else {
-    res.status(200).json({ users: ['John Doe'] })
+  switch (method) {
+    case 'POST':
+      // check the req param if it is ok
+      const { email, passward } = req.body
+      // create the user
+      // send the response
+      break
+    default:
+      res.setHeader('Allow', ['POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
 
 export default handler
+
+// if (req.method === 'POST') {
+//   try {
+//     assert.notEqual(null, req.body.email, 'Email required')
+//     assert.notEqual(null, req.body.password, 'Password required')
+//   } catch (bodyError) {
+//     res.status(403).json({ error: true, message: bodyError.message })
+//   }
+
+//   client.connect(function (err) {
+//     assert.equal(null, err)
+//     console.log('Connected to MongoDB server =>')
+//     const db = client.db(dbName)
+//     const email = req.body.email
+//     const password = req.body.password
+
+//     findUser(db, email, function (err, user) {
+//       if (err) {
+//         res.status(500).json({ error: true, message: 'Error finding User' })
+//         return
+//       }
+
+//       if (!user) {
+//         createUser(db, email, password, function (creationResult) {
+//           console.log(creationResult)
+//           if (creationResult.ops?.length === 1) {
+//             const user = creationResult.ops[0]
+//             const token = jwt.sign(
+//               { userId: user.userId, email: user.email },
+//               jwtSecret,
+//               {
+//                 expiresIn: '1m',
+//               }
+//             )
+//             res.status(200).json({ token })
+//             return
+//           }
+//         })
+//       } else {
+//         res.status(403).json({ error: true, message: 'Email exists' })
+//         return
+//       }
+//     })
+//   })
+// } else {
+//   res.status(200).json({ users: ['John Doe'] })
+// }
