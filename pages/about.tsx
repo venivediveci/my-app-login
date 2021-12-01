88736245212286fe356740b5ec33dc0335f6dd22
coderhh <henry.h.hao@foxmail.com>
2021-12-01 23:14:57 +0800

    title

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 
 const About: React.FC = () => {
+  const [title, setTitle] = useState('About')
   return (
     <div>
       <Head>
-        <title>About</title>
+        <title>{title}</title>
       </Head>
       <h1>About</h1>
+      <button onClick={() => setTitle(title === 'About' ? 'Not' : 'About')}>
+        change title
+      </button>
       <Link href="/login">Login</Link>
     </div>
   )
