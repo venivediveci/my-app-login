91263f06f0007f33298a4ac255ceb401a8a4db06
coderhh <henry.h.hao@foxmail.com>
2021-12-02 23:08:55 +0800

    add page

---
+++ pages/data-visualization.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useState } from 'react'
+
+const DataVisualization: React.FC = () => {
+  const [title, setTitle] = useState('Data Visualization')
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1>About</h1>
+      <button
+        onClick={() =>
+          setTitle(
+            title === 'Data Visualization' ? 'Not' : 'Data Visualization'
+          )
+        }
+      >
+        change title
+      </button>
+      <Link href="/login">Login</Link>
+    </div>
+  )
+}
+
+export default DataVisualization
